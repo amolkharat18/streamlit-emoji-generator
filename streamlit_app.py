f04d8a330d5f90bc344f00982e78ac4f53096173
Amol Kharat <amolkharat@yahoo.com>
2024-11-18 06:40:38 +0000

    Final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+st.title("Bollywood Song Emoji Generator")
+
+input = st.text_input("Generate emoji for:")
+
+system_prompt = """
+    You will be provided with a Bollywood song in Hindi or Hinglish, and your task is to translate it into emojis. Do not use any regular text. Do your best with emojis only.
+"""
+
+if st.button("Generate") and input:
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": input},
+        ],    
+        max_tokens=200
+    )
+    st.text(response.choices[0].message.content)
